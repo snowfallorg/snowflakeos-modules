dbeb6462867f6841d6bf35e8ede3c1e7f8ec8899
Kevin Shenk <kevin@avu.nu>
2023-12-27 09:44:18 -0500

    Update git urls to snowfallorg

--- flake.nix
+++ flake.nix
@@ -4,19 +4,19 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nix-software-center = {
-      url = "github:vlinkz/nix-software-center";
+      url = "github:snowfallorg/nix-software-center";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-conf-editor = {
-      url = "github:vlinkz/nixos-conf-editor";
+      url = "github:snowfallorg/nixos-conf-editor";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     snow = {
-      url = "github:snowflakelinux/snow";
+      url = "github:snowfallorg/snow";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     snowflakeos-module-manager = {
-      url = "github:snowflakelinux/snowflakeos-module-manager";
+      url = "github:snowfallorg/snowflakeos-module-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     snowfall-lib = {
